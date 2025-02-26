67926819ffd7937cd75cdabe8cac5fc5bf3cba88
TEJASHWIN S <144519865+tejash2004re@users.noreply.github.com>
2025-02-25 20:12:24 -0800

    Create LAB-1.php

---
+++ LAB-1.php
@@ -0,0 +1,7 @@
+<?php
+ $f = 1; // Initialize $f as 1 for multiplication
+ for($i = 1; $i <= 5; $i++) {
+     $f = $f * $i; // Calculate factorial
+ }
+ echo "Factorial = " . $f; // Output the result
+?>
